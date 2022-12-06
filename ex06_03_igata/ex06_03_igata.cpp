bd9d2a264f3cf188e5363dea9a0c5711956e9ce5
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-12-06 16:36:10 +0900

    プロジェクト ファイルを追加します。

---
+++ ex06_03_igata/ex06_03_igata.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+class Box {
+public:
+	float width, height, depth;
+
+	Box(float width, float height, float depth)
+	{
+		this->width = width;
+		this->height = height;
+		this->depth = depth;
+	}
+	float GetVolume()
+	{
+		return width * height * depth;
+	}
+	float GetSurface()
+	{
+		return (width * height + height * depth + depth * width) * 2;
+	}
+};
+int Main()
+{
+	float width, height, depth;
+	cout << "haba" << endl;
+	cin >> width;
+	cout << "takasa" << endl;
+	cin >> height;
+	cout << "okuyuki" << endl;
+	cin >> depth;
+	Box* box = new Box(width, height, depth);
+	cout << "boxの表面積" << box->GetSurface() << "boxの体積 " << box->GetVolume();
+	delete box;
+}
